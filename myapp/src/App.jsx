3fdc607bfec4aad7e9c4fb1cc2489af8478df458
Sanjoy Saha <76898740+SanjoySaha24@users.noreply.github.com>
2023-01-12 16:20:52 +0530

    files added

---
+++ myapp/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import "./styles/App.css"
+import "./styles/Header.css"
+
+const App = () => {
+  return(
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
