66f848370315c0239f62007deb71903269efd6a3
Sanjoy Saha <76898740+SanjoySaha24@users.noreply.github.com>
2023-01-13 12:35:05 +0530

    files added

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -2,11 +2,26 @@ import React from 'react'
 import Header from './components/Header'
 import "./styles/App.css"
 import "./styles/Header.css"
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Product from "./components/Product"
 
 const App = () => {
   return(
     <>
-      <Header />
+    <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product/id" element={<Product />} />
+          <Route path="*" element={<div>Page not found 404</div>} />
+          
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ myapp/src/components/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <>
+        <div>
+            <h1>#productid</h1>
+        </div>
+    </>
+  )
+}
+
+export default Product
